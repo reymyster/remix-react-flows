4be743c0640fe01059683c4e6f18ff4a9e67ecd2
Rey Trajano <rey@reytrajano.com>
2025-02-18 18:54:18 -0800

    first layouts

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,14 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +18,36 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="h-screen w-screen p-2">
+      <Tabs defaultValue="one" className="max-w-7xl mx-auto">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="one">One</TabsTrigger>
+          <TabsTrigger value="two">Two</TabsTrigger>
+        </TabsList>
+        <TabsContent value="one">
+          <Card>
+            <CardHeader>
+              <CardTitle>First Test</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="h-[calc(100svh_-_162px)] w-full bg-red-500">
+                hi
+              </div>
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="two">
+          <Card>
+            <CardHeader>
+              <CardTitle>Second Test</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div>hi</div>
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 }
