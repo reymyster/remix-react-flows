0fad1dc298395a3b3f2d3a4bc4d3f1078a25e2ab
Rey Trajano <rey@reytrajano.com>
2025-02-18 18:57:49 -0800

    init react-flow

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,18 @@
 import type { Route } from "./+types/home";
 
 import {
+  ReactFlow,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  BackgroundVariant,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+
+import {
   Card,
   CardContent,
   CardDescription,
@@ -17,7 +29,16 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
 export default function Home() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
   return (
     <div className="h-screen w-screen p-2">
       <Tabs defaultValue="one" className="max-w-7xl mx-auto">
@@ -32,7 +53,22 @@ export default function Home() {
             </CardHeader>
             <CardContent>
               <div className="h-[calc(100svh_-_162px)] w-full bg-red-500">
-                hi
+                <ReactFlow
+                  nodes={nodes}
+                  edges={edges}
+                  onNodesChange={onNodesChange}
+                  onEdgesChange={onEdgesChange}
+                  colorMode="dark"
+                  fitView
+                >
+                  <Controls />
+                  <MiniMap />
+                  <Background
+                    variant={BackgroundVariant.Dots}
+                    gap={12}
+                    size={1}
+                  />
+                </ReactFlow>
               </div>
             </CardContent>
           </Card>
